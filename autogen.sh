52eba2fda5fb7fc5048f50c38bfe87ffb7d96c9f
Rob Bradford <rob@linux.intel.com>
2010-06-21 12:05:44 +0100

    Moblin->MeeGo transition

--- autogen.sh
+++ autogen.sh
@@ -4,13 +4,13 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="moblin-panel-people"
+PKG_NAME="meego-panel-people"
 REQUIRED_AUTOMAKE_VERSION=1.10
 
 (test -f $srcdir/configure.ac \
   && test -d $srcdir/src) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level moblin-panel-people directory"
+    echo " top-level meego-panel-people directory"
     exit 1
 }
 
